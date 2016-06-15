9dc32ac4212921218a942d65f571e4349aee244c
Charlie Jaime <baroonie10@gmail.com>
2016-06-15 03:27:39 -0700

    function for paddle player control added

--- js/script.js
+++ js/script.js
@@ -2,10 +2,25 @@
 var canvas;
 var canvasContext;
 var ballX = 50;
-var ballSpeedX = 5;
-var ballY;
+var ballY = 50;
+var ballSpeedX = 10;
+var ballSpeedY = 4;
+var paddle1Y = 210;
+const PADDLE_HEIGHT = 100;
 
 // Main Game Logic
+
+function calMousePos(evt){
+  var rect = canvas.getBoundingClientRect();
+  var root = document.documentElement;
+  var mouseX = evt.clientX - rect.left - root.scrollLeft;
+  var mouseY = evt.clientY - rect.top - root.scrollTop;
+  return{
+    x:mouseX,
+    y:mouseY
+  };
+}
+
 window.onload = function(){
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
@@ -15,17 +30,30 @@ window.onload = function(){
     movement();
     playArea();
   }, 1000/framesPerSecond);
+
+  canvas.addEventListener('mousemove', function(evt){
+    var mousePos = calMousePos(evt);
+    paddle1Y = mousePos.y-(PADDLE_HEIGHT/2);
+  });
 }
 
 // Functions for game
 function movement(){
   // main function for ball movement
   ballX = ballX + ballSpeedX;
+  ballY = ballY + ballSpeedY;
+
   if(ballX < 0){
     ballSpeedX = -ballSpeedX;
   }
   if(ballX > canvas.width){
     ballSpeedX = -ballSpeedX;
+  }
+  if(ballY < 0){
+    ballSpeedY = -ballSpeedY;
+  }
+  if(ballY > canvas.height){
+    ballSpeedY = -ballSpeedY;
   }
 }
 
@@ -40,9 +68,9 @@ function playArea(){
   // canvas
   colorRect(0,0,canvas.width,canvas.height,'lightsteelblue');
   // left paddle
-  colorRect(0,210,10,100,'midnightblue');
+  colorRect(0,paddle1Y,10,PADDLE_HEIGHT,'midnightblue');
   // right paddle
   colorRect(790,210,10,100,'midnightblue');
   // ball
-  colorRect(ballX,280,20,20,'midnightblue');
+  colorRect(ballX,ballY,20,20,'midnightblue');
 }
