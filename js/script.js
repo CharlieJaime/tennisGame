ce2afb0f12d72f7814ab51391c5903d861ad3490
Charlie Jaime <baroonie10@gmail.com>
2016-06-15 16:48:50 -0700

    improved ball control and added winning functionality

--- js/script.js
+++ js/script.js
@@ -14,7 +14,8 @@ const PADDLE_THICKNESS = 15;
 // scores
 var score1 = 0;
 var score2 = 0;
-
+const WIN_SCORE = 3;
+var showWin = false;
 // Main Game Logic
 
 function calMousePos(evt){
@@ -55,6 +56,10 @@ function aiPaddle(){
 }
 
 function movement(){
+  if(showWin){
+    return;
+  }
+
   aiPaddle();
   // main function for ball movement
   ballX += ballSpeedX;
@@ -63,17 +68,19 @@ function movement(){
   if(ballX < 0){
     if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT){
       ballSpeedX = -ballSpeedX;
+      var deltaY = ballY - (paddle1Y+PADDLE_HEIGHT/2);
+      ballSpeedY = deltaY * 0.35;
     } else{
-      ballRest();
       score2 += 1;
+      ballRest();
     }
   }
   if(ballX > canvas.width){
     if(ballY > paddle2Y && ballY < paddle2Y+PADDLE_HEIGHT){
       ballSpeedX = -ballSpeedX;
     } else{
-      ballRest();
       score1 += 1;
+      ballRest();
     }
   }
   if(ballY < 0){
@@ -94,6 +101,13 @@ function colorRect(leftX, topY, width, height, color){
 function playArea(){
   // canvas
   colorRect(0,0,canvas.width,canvas.height,'lightsteelblue');
+
+  if(showWin){
+    canvasContext.fillStyle = 'midnightblue';
+    canvasContext.fillText('Click to Continue', 100, 100);
+    return;
+  }
+
   // left paddle
   colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'midnightblue');
   // right paddle
@@ -106,6 +120,11 @@ function playArea(){
 }
 
 function ballRest(){
+  if (score1 >= WIN_SCORE || score2 >= WIN_SCORE) {
+    score1 = 0;
+    score2 = 0;
+    showWin = true;
+  }
   ballSpeedX = -ballSpeedX;
   ballX = canvas.width/2;
   ballY = canvas.height/2;
