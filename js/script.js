54ce23f4eef02e93e9b596ba59e1627604ab03b9
Charlie Jaime <baroonie10@gmail.com>
2016-06-14 22:43:03 -0700

    add bounce to ball and refactored drawing code

--- js/script.js
+++ js/script.js
@@ -1,32 +1,48 @@
 // Gloabl Vars
 var canvas;
 var canvasContext;
-var ballX;
+var ballX = 50;
+var ballSpeedX = 5;
 var ballY;
 
 // Main Game Logic
 window.onload = function(){
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
+
   var framesPerSecond = 30;
   setInterval(function(){
     movement();
-    drawEverything();
+    playArea();
   }, 1000/framesPerSecond);
 }
 
 // Functions for game
 function movement(){
-  ballX = ballX + 5;
+  // main function for ball movement
+  ballX = ballX + ballSpeedX;
+  if(ballX < 0){
+    ballSpeedX = -ballSpeedX;
+  }
+  if(ballX > canvas.width){
+    ballSpeedX = -ballSpeedX;
+  }
 }
 
-function drawEverything(){
-  canvasContext.fillStyle = 'lightsteelblue';
-  canvasContext.fillRect(0,0,canvas.width,canvas.height);
-  canvasContext.fillStyle = 'midnightblue';
-  canvasContext.fillRect(0,210,10,100);
-  canvasContext.fillStyle = 'midnightblue';
-  canvasContext.fillRect(790,210,10,100);
-  canvasContext.fillStyle = 'steelblue';
-  canvasContext.fillRect(380,280,20,20);
+// use this color for divison lines steelblue
+function colorRect(leftX, topY, width, height, color){
+  // main function to draw on canvas and maintain DRY
+  canvasContext.fillStyle = color;
+  canvasContext.fillRect(leftX,topY,width,height);
+}
+
+function playArea(){
+  // canvas
+  colorRect(0,0,canvas.width,canvas.height,'lightsteelblue');
+  // left paddle
+  colorRect(0,210,10,100,'midnightblue');
+  // right paddle
+  colorRect(790,210,10,100,'midnightblue');
+  // ball
+  colorRect(ballX,280,20,20,'midnightblue');
 }
