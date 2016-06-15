2a81f4edba6894fad66a5c246c02c380541fb9c0
Charlie Jaime <baroonie10@gmail.com>
2016-06-15 05:20:35 -0700

    added ai to right paddle and scoring function/display

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 // Gloabl Vars
 var canvas;
 var canvasContext;
+// Balls
 var ballX = 50;
 var ballY = 50;
 var ballSpeedX = 10;
 var ballSpeedY = 4;
+// paddles
 var paddle1Y = 210;
 var paddle2Y = 210;
 const PADDLE_HEIGHT = 100;
 const PADDLE_THICKNESS = 15;
+// scores
+var score1 = 0;
+var score2 = 0;
 
 // Main Game Logic
 
@@ -40,16 +45,27 @@ window.onload = function(){
 }
 
 // Functions for game
+function aiPaddle(){
+  var paddle2YCen = paddle2Y + (PADDLE_HEIGHT/2);
+  if(paddle2YCen < ballY-35){
+    paddle2Y = paddle2Y + 10;
+  } else if(paddle2YCen > ballY+35){
+    paddle2Y = paddle2Y - 10;
+  }
+}
+
 function movement(){
+  aiPaddle();
   // main function for ball movement
-  ballX = ballX + ballSpeedX;
-  ballY = ballY + ballSpeedY;
+  ballX += ballSpeedX;
+  ballY +=ballSpeedY;
 
   if(ballX < 0){
     if(ballY > paddle1Y && ballY < paddle1Y+PADDLE_HEIGHT){
       ballSpeedX = -ballSpeedX;
     } else{
       ballRest();
+      score2 += 1;
     }
   }
   if(ballX > canvas.width){
@@ -57,6 +73,7 @@ function movement(){
       ballSpeedX = -ballSpeedX;
     } else{
       ballRest();
+      score1 += 1;
     }
   }
   if(ballY < 0){
@@ -83,6 +100,9 @@ function playArea(){
   colorRect(canvas.width-PADDLE_THICKNESS,paddle2Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'midnightblue');
   // ball
   colorRect(ballX,ballY,20,20,'midnightblue');
+  // scores
+  canvasContext.fillText(score1, 100, 100);
+  canvasContext.fillText(score2, canvas.width-100, 100);
 }
 
 function ballRest(){
