39593b545d23e4b12ad9d45949545b105dfd80cb
Charlie Jaime <baroonie10@gmail.com>
2016-06-15 18:26:00 -0700

    win displays, net drawing, and reset click functions added

--- js/script.js
+++ js/script.js
@@ -29,6 +29,14 @@ function calMousePos(evt){
   };
 }
 
+function handleMouseClick(evt){
+  if(showWin){
+    score1 = 0;
+    score2 = 0;
+    showWin = false;
+  }
+}
+
 window.onload = function(){
   canvas = document.getElementById('gameCanvas');
   canvasContext = canvas.getContext('2d');
@@ -38,6 +46,8 @@ window.onload = function(){
     movement();
     playArea();
   }, 1000/framesPerSecond);
+
+  canvas.addEventListener('mousedown', handleMouseClick);
 
   canvas.addEventListener('mousemove', function(evt){
     var mousePos = calMousePos(evt);
@@ -98,16 +108,30 @@ function colorRect(leftX, topY, width, height, color){
   canvasContext.fillRect(leftX,topY,width,height);
 }
 
+function Net(){
+  for (var i = 0; i < canvas.height; i+=40) {
+    colorRect(canvas.width/2-1, i, 2, 20, 'midnightblue');
+  }
+}
+
 function playArea(){
   // canvas
   colorRect(0,0,canvas.width,canvas.height,'lightsteelblue');
 
   if(showWin){
     canvasContext.fillStyle = 'midnightblue';
-    canvasContext.fillText('Click to Continue', 100, 100);
+    if(score1 >= WIN_SCORE){
+      canvasContext.fillText('You Won!', 100, 150);
+    }else if(score2 >= WIN_SCORE){
+      canvasContext.fillText('Computer Won!', canvas.width-100, 150);
+    }
+    canvasContext.fillStyle = 'midnightblue';
+    canvasContext.fillText('Click to Continue', 400, 300);
     return;
   }
 
+  // net drawwing
+  Net();
   // left paddle
   colorRect(0,paddle1Y,PADDLE_THICKNESS,PADDLE_HEIGHT,'midnightblue');
   // right paddle
@@ -121,8 +145,6 @@ function playArea(){
 
 function ballRest(){
   if (score1 >= WIN_SCORE || score2 >= WIN_SCORE) {
-    score1 = 0;
-    score2 = 0;
     showWin = true;
   }
   ballSpeedX = -ballSpeedX;
